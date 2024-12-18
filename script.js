4cc810b6adce110fe2a88c5d6815ad4259ad8a15
Francesco Del Giudice <38216012+francescodelgiudice@users.noreply.github.com>
2024-12-18 14:48:50 +0100

    Refactor

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
 
   setTimeout(function () {
     document.querySelector('#boot-ready').style.display = 'none'
+    manageStartButton()
   }, 3000)
 
   const playButton = document.getElementById('play-and-pause')
@@ -15,11 +16,34 @@ window.addEventListener('load', () => {
 })
 
 window.scrollTo(0, 1)
+
 function playAndPauseAudio () {
   const audio = document.getElementById('audio')
   if (audio.paused) {
     audio.play()
   } else {
     audio.pause()
+  }
+}
+
+function manageStartButton() {
+  const startButton = document.getElementById('start-button')
+  const startMenu = document.getElementById('start-menu')
+  const shutDownButton = document.getElementById('shutdown-button')
+
+  if (startButton && startMenu) {
+    startButton.addEventListener('click', () => {
+      if (startMenu.style.display === 'block') {
+        startMenu.style.display = 'none'
+        return
+      } else {
+        startMenu.style.display = 'block'
+      }
+    })
+  }
+  if (shutDownButton) {
+    shutDownButton.addEventListener('click', () => {
+      window.location.reload()
+    })
   }
 }
